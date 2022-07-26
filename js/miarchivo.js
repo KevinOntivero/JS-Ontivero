89fbe4fc7f32e85f77b42ca1467507879c03a069
Kevin Web <keviinontiivero00@gmail.com>
2022-07-26 19:10:29 -0300

    Array desafio

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,3 +1,15 @@
+/* array */
+let pasajes_list = ["Belgica", "Canada", "Nevada", "Finlandia", "España", "Italia", "Roma", "Francia"];
+pasajes_list.unshift("Mexico");
+let pasajes = prompt("Ingrese destino: ('Belgica', 'Canada', 'Nevada', 'Finlandia', 'España', 'Italia', 'Roma', 'Francia' 'Mexico'")
+while(pasajes != "Belgica" && pasajes != "Canada" && pasajes != "Nevada" && pasajes != "Finlandia" && pasajes != "España" && pasajes != "Italia" && pasajes != "Roma" && pasajes != "Francia" && pasajes != "Mexico" ){
+    pasajes = prompt("Confirme su destinto")
+}
+alert("Elegiste: " + pasajes)
+
+
+/* simulador de cuotas */
+
 let valorTotal = 15000;
 let cuotaPagar = prompt("Escriba las cuotas a pagar (6, 12, 18, 24)");
 
@@ -6,58 +18,5 @@ while(cuotaPagar != 6 && cuotaPagar != 12 && cuotaPagar != 18 && cuotaPagar != 2
 }
 
 let valorFinal = valorTotal / cuotaPagar;
-alert ("Tus cuotas son " + cuotaPagar + "cuotas, y el valor de cada cuota sera de:" + valorFinal)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* let hola = prompt("Bienvenido! Ingresa 'hola' para comenzar");
-while(hola != "hola" ){
-    alert("Has ingresado " + hola);
-    hola = prompt("Para comenzar ingresa 'hola'");
-}
-
-let ver = prompt("Un gusto, quisieras ver todo el contenido? responde con un 'si' si asi lo quisieras")
-while(ver != "si"){
-    alert("Ingresaste " + ver)
-    ver = prompt("Por favor indica con 'si' para seguir")
-}
-
-let prueba = prompt("Perfecto! para seguir solo necesito dos pasos mas, ingresa un 'no soy' si no eres un robot")
-while (prueba != "no soy"){
-    alert("Ingresaste " + prueba)
-    prueba = prompt ("Por favor, indicar 'no soy' si no eres un robot")
-}
-
-let finish = prompt("Un ultimo paso, ingrese 'seguir' para poder continuar viendo nuestra pagina")
-while (finish != "seguir"){
-    alert("Ingresaste " + finish)
-    finish = prompt("Por favor, para continuar a nuestra pagina ingresar 'seguir'")
-} */
-
-
-
-
-
-
-
-
+alert ("Tus cuotas son: " + cuotaPagar + " cuotas, y el valor de cada cuota sera de: " + valorFinal)
 
